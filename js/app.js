52fba82bdc6193ef0812ec3b336996b7af0aaca4
luapbbor <luapbbor@gmail.com>
2020-08-25 13:06:06 +1000

    html and css initial config

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// global variables
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
